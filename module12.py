5d92f0f17214b0d62a32334dce3292c567d7845a
utta <sakari05salminen@gmail.com>
2025-11-13 11:12:59 +0200

    ignore+ commet

--- module12.py
+++ module12.py
@@ -3,3 +3,5 @@ import requests
 response = requests.get("https://api.chucknorris.io/jokes/random")
 
 print(response.json()["value"])
+
+# onkohan pakko tehdä tota 2 tehtävää kun ei tekis mieli tehä käyttäjää
